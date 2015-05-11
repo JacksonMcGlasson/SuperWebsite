8ed2d4d6244ded34e860cec67ee8f3d7dcc12c14
Jackson McGlasson <originalqwerty45@gmail.com>
2015-05-11 10:43:54 -0700

    added to more stories to article 1

--- article1.php
+++ article1.php
@@ -45,16 +45,25 @@ and open the template in the editor.
                             <h3>These are 10 wierd stories that caught attention</h3>
                             <p>1.Thief steals a fridge from a gas station<br />
                                 <br />
-                                 A man walked in to a gas station convenience store at 4:09am April 2 and noticed that the<br />
-                               the clerk was asleep. He then decided he would take 15 minutes to pry an ice cream freezer from the wall<br />
-                               and take it with him. he was arrested a few days later.</p> 
+                                 A man walked in to a gas station convenience store at 4:09am April 2 and noticed that the
+                               clerk was asleep. He then decided he would take 15 minutes to pry an ice cream freezer from the wall
+                               and take it with him. He was arrested a few days later. Not many people would steal such an unvaluable
+                               thing.</p> 
                             <img src="http://media.skynews.com/media/images/generated/2015/4/24/386933/default/v1/fridgees-1-762x428.jpg">
-                            <p>2.Something<br />
+                            <p>2.Very incompetent thief<br />
                                 <br />
-                               something is something</p> 
-                            <p>3.Something<br />
+                                 If that last one was not entertaining enough here is another story about a thief.
+                               A man named Jamie Brown stole a fish tank from a hardware store that he was banned from.
+                               While he was running from the cops he decided to stop to urinate in a bush. Unfortunately for him,
+                               there was wasps nest in the bush. He then had to be rescued by the police and spend six unpleasant 
+                               hours in the hospital.</p>  
+                            <img src="http://static.socialitelife.com/uploads/2013/01/24/stephen-dorff-peeing-bushes-01242013-05-435x580.jpg">
+                            <p>3.Omelet accident<br />
                                 <br />
-                               something is something</p> 
+                                 Three tractor trailers crashed on an interstate in Chattanooga, Tennessee. Out of each truck
+                               spilled a load of eggs, pallets of cheese, and boxes of meat. This created a giant omelet on 
+                               the highway.It took hours to clean up.</p> 
+                            <img src="http://wac.450f.edgecastcdn.net/80450F/999ktdy.com/files/2012/06/Eggs.png">
                             <p>4.Something<br />
                                 <br />
                                something is something</p> 
