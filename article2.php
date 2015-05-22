d072b99049b51c536a1f2396a5bad28fceee64a8
Jackson McGlasson <originalqwerty45@gmail.com>
2015-05-22 08:37:35 -0700

    finished list article 2

--- article2.php
+++ article2.php
@@ -30,7 +30,7 @@ and open the template in the editor.
             <div class="row">
                 <div class="col-md-10">
                     <div class="row">
-                        <div class="col-xs-2"> <h1> 10 Recent Animal Extinctions </h1>
+                        <div class="col-xs-2"> <h1> 7 Recent Animal Extinctions </h1>
                         </div>
                         <div class="col-xs-10">
 
@@ -43,49 +43,50 @@ and open the template in the editor.
                             <h3>Here is a list of animal's that became extinct relatively recently thanks to humans. Feel bad.</h3>
                             <p>1.Baiji River Dolphin<br />
                                 <br />
-                                 As China industrialized and started to use the rivers more, this dolphin started dying.Waste flowed into the river,
-                               and parts of it were replaced with concrete. Excessive boat travel and dangerous fishing techniques killed them quickly
-                               in the 70's and 80's. They became officially extinct after not a single one could be found in 2006.</p> 
+                                As China industrialized and started to use the rivers more, this dolphin started dying.Waste flowed into the river,
+                                and parts of it were replaced with concrete. Excessive boat travel and dangerous fishing techniques killed them quickly
+                                in the 70's and 80's. They became officially extinct after not a single one could be found in 2006.</p> 
                             <img src="http://i0.wp.com/listverse.com/wp-content/uploads/2009/07/baiji-tm.jpg?resize=400%2C213">
                             <p>2.Javan Tiger<br />
                                 <br />
-                                 In the early 19th century, the Javan Tiger was so common it was considered a pest. As human population increased their
-                               habitat was cleared for farmland. They were hunted down and poisoned to make room. They disappeared between the 1970's
-                               and 80's.
-                                 </p>  
+                                In the early 19th century, the Javan Tiger was so common it was considered a pest. As human population increased their
+                                habitat was cleared for farmland. They were hunted down and poisoned to make room. They disappeared between the 1970's
+                                and 80's.
+                            </p>  
                             <img src="http://i2.wp.com/listverse.com/wp-content/uploads/2009/07/javan_tiger-tm.jpg?resize=400%2C273g">
                             <p>3.Golden Toads<br />
                                 <br />
-                                 The golden toad was first discovered in 1966. By 1990 they all disappeared. It lived in Costa Rica and there numbers were
-                               pretty steady until 1987. Because of strange weather, only 29 out of a possible 30,000 toads were born. That number just 
-                               trickled down over the next few years.</p> 
+                                The golden toad was first discovered in 1966. By 1990 they all disappeared. It lived in Costa Rica and there numbers were
+                                pretty steady until 1987. Because of strange weather, only 29 out of a possible 30,000 toads were born. That number just 
+                                trickled down over the next few years.</p> 
                             <img src="http://i1.wp.com/listverse.com/wp-content/uploads/2009/07/img_18.jpg">
-                            <p>4.Expensive Toilets<br />
+                            <p>4.Passenger Pigeon<br />
                                 <br />
-                                 People sometimes think that if something is owned by a celebrity it needs to be sold for a lot of
-                               money. In some cases, people want to buy celebrity toilets for a large cash sum. One of John Lennon's toilets 
-                               sold for 9,500 pounds(about $14,740) at an auction. Someone also decided to open bids on the toilet of J.D Salinger,
-                               an author. the bids started at 1 million dollars because the dealer said "who knows how  many of Salinger's stories
-                               were thought up and written while he sat on this throne!" These sound like great ways to spend your money.</p> 
-                            <img src="http://blog-images.homethangs.com/2011/10/FloWise-Dual-Flush-Elongated-Toilet1-300x300.jpg">
-                            <p>5.Something<br />
+                                200 years ago, the passenger pigeon was the most common bird in North America. They flew in flocks that could number in billions.
+                                Pigeon meat started to be advertized as cheap food. Slaves and the poor ate them a lot. This led to a massive hunting campaign. Farmers 
+                                thought they were threats because of their numbers. The last passenger died in a zoo on September 1,1914.
+                            </p> 
+                            <img src="http://i2.wp.com/listverse.com/wp-content/uploads/2009/07/passengerpigeon.jpg">
+                            <p>5.Quagga<br />
                                 <br />
-                               something is something</p> 
-                            <p>6.Something<br />
+                                The quagga was a subspecies of zebra. It had stripes only on its head, neck and chest. The last wild quaggas were caught by 1870.
+                                the very last quagga died in a zoo in 1883. However, people did not know they were extinct; people indiscriminatly called anytype of
+                                zebra a quagga. It went extinct because of ruthless hunting.</p> 
+                            <img src="http://i1.wp.com/listverse.com/wp-content/uploads/2009/07/quagga_photo.jpg">
+                            <p>6.Thyacline<br />
                                 <br />
-                               something is something</p> 
-                            <p>7.Something<br />
+                                The thyacline or tasmanian tiger was the largest carniverous marsupial. They were wiped out because of constant hunting. They were hunted 
+                               down because they attacked sheep. People also started settling on their habitat. It was only recognized as being endangered the year the last 
+                               one died. It died because it was locked outside in its zoo pen and froze to death.</p> 
+                            <img src="http://i2.wp.com/listverse.com/wp-content/uploads/2009/07/picture-1-112.png">
+                            <p>7.West African Black Rhino<br />
                                 <br />
-                               something is something</p> 
-                            <p>8.Something<br />
-                                <br />
-                               something is something</p>
-                            <p>9.Something<br />
-                                <br />
-                               something is something</p> 
-                            <p>10.Something<br />
-                                <br />
-                               something is something</p> 
+                                The rarest of the black rhino subspecies, the West African black rhinoceros is currently recognized as "critically endangered," but researchers
+                                fear it may be extinct. The species was once widespread in central Africa, but the population has been in decline due to poaching. By 1980, the 
+                                population was in the hundreds, and by 2000 only an estimated 10 rhinos remained. A survey of the animal's last remaining habitat in northern Cameroon 
+                                failed to find any of the rhinos, but search efforts continue. No West African black rhinos are known to be held in captivity.</p> 
+                            <img src="http://media.mnn.com/sites/default/files/styles/featured_blog/public/rhino_2.jpg">
+                            c="http://blog-images.homethangs.com/2011/10/FloWise-Dual-Flush-Elongated-Toilet1-300x300.jpg">
                         </div>
 
                     </div>
--- view/navigation2.php
+++ view/navigation2.php
@@ -7,7 +7,7 @@ require_once (__DIR__ . "/../model/config.php");
 
         <li><a href="<?php echo $path . "gallery.php"; ?>">Gallery</a></li>
         <li><a href="<?php echo $path . "article1.php"; ?>">10 Weird Stories</a></li>
-        <li><a href="<?php echo $path . "article2.php"; ?>">10 Recent Animal Extinctions </a></li>
+        <li><a href="<?php echo $path . "article2.php"; ?>">7 Recent Animal Extinctions </a></li>
         <li><a href="<?php echo $path . "article3.php"; ?>">Another List </a></li>
     </ul>
 </nav>
